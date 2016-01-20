f8e572b113fadcaa34d1fc859ca1206d7210112f
esc <esc@youhavethewrong.info>
2016-01-20 08:23:58 -0600

    Manage ring service with mount.

--- src/hacker_snooze/www.clj
+++ src/hacker_snooze/www.clj
@@ -1,14 +1,21 @@
 (ns hacker-snooze.www
   (:require [compojure.core :refer [routes defroutes GET]]
             [compojure.handler :as handler]
-            [ring.adapter.jetty :refer [run-jetty]]))
+            [ring.adapter.jetty :refer [run-jetty]]
+            [mount.core :as mount]
+            [hacker-snooze.db :as db]))
 
-(defroutes hn-routes
-  (GET "/" [] "Welcome" ))
+(defroutes www-routes
+  (GET "/" [] "Welcome" )
+  (GET "/articles" [] (db/all-articles db/hn)))
 
-(defn start-hn
+(defn start-www
   []
-  (-> (routes hn-routes)
+  (-> (routes www-routes)
       (handler/site)
       (run-jetty {:join? false
                   :port 8081})))
+
+(mount/defstate www-app
+  :start (start-www)
+  :stop (.stop www-app))
